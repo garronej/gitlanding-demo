6db4c12fdf86c30fa75286850c2ef2873a4050f4
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-09-16 00:09:59 +0200

    Setup gitlanding

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { OnyxiaUi } from "./theme";
+import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <OnyxiaUi>
-            <h1>Hello World!</h1>
-        </OnyxiaUi>
+        <App />
     </StrictMode>
 );
